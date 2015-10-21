eb17bcae84930f76e8a1327975e99477c6622aa2
DenizUgur <deniz343@gmail.com>
2015-10-21 23:39:50 +0300

    ajax result.html

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    console.log("ready!");
+    
+    $.ajax({
+        url: "result.html",
+        dataType: "html"
+    }).done(function (responseHtml) {
+        $("#results").html(responseHtml);
+    });
+
+    $("#submit").click(function () {
+        Materialize.toast('Hello!', 3000, 'rounded');
+
+        var parent = $(".parent-element");
+        var position = parent.position();
+        var right = position.left + parent.width();
+        Materialize.toast("left: " + position.left + ", right: " + right, 3000, 'rounded');
+
+        $("#results").empty();
+        $.ajax({
+            url: "result.html",
+            dataType: "html"
+        }).done(function (responseHtml) {
+            $("#results").html(responseHtml);
+            $("#hr-sitemap").animate({
+                width: "+=560"
+            }, 500, function () {
+                // Animation complete.
+                Materialize.toast('Completed', 3000, 'rounded');
+            });
+            $(".chip-section").animate({
+                width: "+=560"
+            }, 500, function () {
+                // Animation complete.
+                Materialize.toast('Completed2', 3000, 'rounded');
+            });
+            $("#result-head-robots").hide('slow', function () {
+                $("#result-head-robots").remove();
+            });
+            $("#results").css({
+                left: position.left,
+                width: right,
+                position: 'absolute'
+            });
+        });
+    });
+});
